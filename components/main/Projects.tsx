124c521eb01db15064d21e0086f2f44f2387d938
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-01-20 17:46:06 +0530

    commit 12

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -3,7 +3,7 @@ import ProjectCrad from '../sub/ProjectCrad'
 
 const Projects = () => {
     return (
-        <div className='flex flex-col items-center justify-center py-20'>
+        <div id ='Projects' className='flex flex-col items-center justify-center py-20'>
             <h1 className='text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20'>
                 My Projects
             </h1>
